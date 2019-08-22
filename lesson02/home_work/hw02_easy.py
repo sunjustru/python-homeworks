b069aac2872c8dc5aa7b335052b4bdc63dbce312
aburov <web@sunjust.ru>
2019-08-23 00:08:42 +0300

    done

--- lesson02/home_work/hw02_easy.py
+++ lesson02/home_work/hw02_easy.py
@@ -11,7 +11,19 @@
 # 3.   киви
 # 4.  арбуз
 
-# Подсказка: воспользоваться методом .format()
+
+fruits_list = ["яблоко", "банан", "киви", "арбуз"]
+
+string_length = 0
+
+##for itm in fruits_list:
+# для нумерации можно использовать i +=1; но решил использовать новый метод
+
+for key, itm in list(enumerate(fruits_list)):
+    if (len(itm) > string_length):
+        string_length = len(itm)
+
+    print(str(key+1)+'.', itm.rjust(string_length))
 
 
 # Задача-2:
@@ -19,7 +31,25 @@
 # Удалите из первого списка элементы, присутствующие во втором списке.
 
 
+first_list  = ["dsadsad", "киви", "арбуз"]
+second_list = ["яблоко", "банан", "киви", "арбуз"]
+
+for itm in first_list:
+    if itm in second_list:
+        first_list.remove(itm)
+print(first_list)
+
 # Задача-3:
 # Дан произвольный список из целых чисел.
 # Получите НОВЫЙ список из элементов исходного, выполнив следующие условия:
 # если элемент кратен двум, то разделить его на 4, если не кратен, то умножить на два.
+
+
+num_list  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+num_res_list = []
+for itm in num_list:
+    if (itm % 2 == 0):
+        num_res_list.append(itm / 4)
+    else:
+        num_res_list.append(itm * 2)
+print(num_res_list)
